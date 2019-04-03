a6ec478ef9d77c183589006dc07b320db032dd5a
carlosclatg <45244247+carlosclatg@users.noreply.github.com>
2019-04-03 13:19:00 +0200

    Add files via upload

---
+++ Spring - dependency injection/src/inversionofcontrol/Informe.java
@@ -0,0 +1,17 @@
+package inversionofcontrol;
+
+public class Informe implements CreacionInformes {
+
+	@Override
+	public String getInformes() {
+		// TODO Auto-generated method stub
+		return "Esta es la presentacion del informe";
+	}
+
+	@Override
+	public String getTareas() {
+		// TODO Auto-generated method stub
+		return "Tareas";
+	}
+
+}
